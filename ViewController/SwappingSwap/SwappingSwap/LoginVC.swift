0d2f3b7ca15775e792e66903cdeb2a1029ca2401
ikamilov <kamilov6@gmail.com>
2017-02-06 12:31:09 -0600

    Swap View Controller

---
+++ ViewController/SwappingSwap/SwappingSwap/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwappingSwap
+//
+//  Created by Islam Kamilov on 2/6/17.
+//  Copyright © 2017 Islam Kamilov. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
